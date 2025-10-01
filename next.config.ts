9e916ed82c4604198ffd0269e674f53eab710285
Aljosa <asrebo@gmail.com>
2025-10-01 13:17:28 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
- crossOrigin: 'anonymous',
+    ignoreDuringBuilds: true,
+ crossOrigin: 'anonymous'
 };
 
 export default nextConfig;
