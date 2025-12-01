c6beb88cfdf0c9f1b7342e0a77afebf99b3af55c
Aljosa <asrebo@gmail.com>
2025-12-01 11:03:48 +0100

    Add new data points to charts and fix layout issue
    Added additional data entries for late November and early December 2025 in barchart_og.tsx, barchart_vo.tsx, and chart-area-interactive.tsx to extend chart coverage. Also fixed a layout issue in obvestila.jsx by removing the 'grid' class from CardContent.

--- components/obvestila.jsx
+++ components/obvestila.jsx
@@ -16,7 +16,7 @@ export default function Obvestila() {
         <CardTitle className="text-2xl font-bold">Obvestila</CardTitle>
         <CardDescription className="text-md">Trenutno ni obvestil</CardDescription>
       </CardHeader>
-      <CardContent className="grid gap-4 flex justify-center items-center h-full">
+      <CardContent className="gap-4 flex justify-center items-center h-full">
         <div className="text-lg text-muted-foreground">
           <img src="mesage.png" alt="No Notifications" className="mx-auto mb-4 w-32 h-32 opacity-50" style={{opacity: 0.1}} />
         </div>
